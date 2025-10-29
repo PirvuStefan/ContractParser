07b2b9a302a0b9995190f2a80fd326ec0e2a1d53
PirvuStefan <silverbench24@gmail.com>
2025-10-29 11:35:28 +0200

    this should be safe

--- src/main/java/org/example/contractparser/Launcher.java
+++ src/main/java/org/example/contractparser/Launcher.java
@@ -1,7 +1,7 @@
 package org.example.contractparser;
 
 public class Launcher {
-    public static void main(String[] args) {
-        HelloApplication.launch(args);
-    }
-}
+       public static void main(String[] args) {
+           javafx.application.Application.launch(HelloApplication.class, args);
+       }
+   }
